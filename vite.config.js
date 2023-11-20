d116571fb57420ab53c2679f8b12ed6a9510e1f9
BarbaraBravoRedondo <barbarabravoredondo@gmail.com>
2023-11-20 16:18:04 +0100

    x

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Owen Wilson's Wow/",
+  base: "/Owen-Wilson-Wow/",
 })
